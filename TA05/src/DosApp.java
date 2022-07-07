8573fca92ad8af1340a9b1a8ca0cc33b5ffd5c2e
Celestinvib <celestinvib@gmail.com>
2022-07-07 09:50:18 +0200

    Ejercicio 3 finalizado

--- TA05/src/DosApp.java
+++ TA05/src/DosApp.java
@@ -1,8 +1,9 @@
-
+import javax.swing.JOptionPane;
 public class DosApp {
 
 	public static void main(String[] args) {
-		String nombre = "Celestin";
+//		String nombre = "Celestin";
+		String nombre = JOptionPane.showInputDialog("Introduce un nombre:");
 		
 		System.out.println("Bienvenido "+nombre);
 	}
