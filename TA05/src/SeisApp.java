fb6dca82015aa672f1c8b822b22479771636a443
Celestinvib <celestinvib@gmail.com>
2022-07-07 10:14:33 +0200

    Ejercicio 6 finalizado

---
+++ TA05/src/SeisApp.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class SeisApp {
+
+	public static void main(String[] args) {
+
+		final double IVA = 0.21;
+		double precioP =Double.parseDouble(JOptionPane.showInputDialog("Introduce el precio del producto:"));
+		
+		JOptionPane.showMessageDialog(null,"Precio con IVA: "+(precioP+(precioP*IVA)));
+	}
+
+}
